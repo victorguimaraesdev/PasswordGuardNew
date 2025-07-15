4a6870c9f94cfdb331bac0a322b0ecb105c51388
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-07-15 14:38:44 -0300

    Arrumando detalhes do card

--- src/components/home/cardLogin.tsx
+++ src/components/home/cardLogin.tsx
@@ -7,12 +7,18 @@ const ContainerCard = styled.div`
     position: relative;
     box-shadow: 4px 3px 3px rgba(0, 0, 0, 0.2);
 `
+const ContainerTitle = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    bottom: 10px;
+    width: 100%;
+    z-index: 2;
+`
 const TitleCard = styled.h3`
     font-size: 20px;
     font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
-    position: absolute;
-    bottom: 10px;
-    left: 50px;
     color: var(--primary);
     z-index: 2;
 `
@@ -25,7 +31,9 @@ export const CardLogin = () => {
     return(
         <ContainerCard>
             <Url src="https://play-lh.googleusercontent.com/1-hPxafOxdYpYZEOKzNIkSP43HXCNftVJVttoo4ucl7rsMASXW3Xr6GlXURCubE1tA=w3840-h2160-rw"></Url>
-            <TitleCard>victor@exemplo.com</TitleCard>
+            <ContainerTitle>
+                <TitleCard>victor@exemplo.com</TitleCard>
+            </ContainerTitle>
         </ContainerCard>
     )
 }
