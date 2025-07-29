8d7b995bb158a82faae0d4c770fc72585992911c
victorguimaraesdev <victorguimararesmax@gmail.com>
2025-07-29 10:45:00 -0300

    feat: handlesubmit for API (register)

--- src/components/home/modalForm.tsx
+++ src/components/home/modalForm.tsx
@@ -1,5 +1,6 @@
+import axios from "axios";
+import { useState } from "react";
 import styled from "styled-components";
-import { formLogin } from "../../utils/infoFormLogin";
 
 const ContainerForm = styled.div `
     display:flex;
@@ -20,48 +21,91 @@ const SubContainerForm = styled.div`
 const TitleForm = styled.h3`
     font-size: 20px;
     color: var(--secondary);
+    margin-bottom: 10px;
 `
 const InputForm = styled.input`
     padding: 5px;
     width: 100%;
     height: 30px;
-    border-radius: 5px;
-    background-color: rgba(247, 246, 246, 0.9);
+    background-color: transparent;
     box-shadow: none;
     border: none;
+    border-bottom: 1px solid white;
+    color: white;
+    &::placeholder {
+        color: white;
+    }
 `
-const PasswordButton = styled.button`
-    width: 100px;
-    height: 30px;
-    background-color: var(--supp2);
-    color: var(--secondary);
-    border: none;
-    border-radius: 5px;
-    cursor: pointer;
-    margin-top: 10px;
-    `
 const RegisterButton = styled.button`
     width: 150px;
     height: 40px;
     background-color: var(--secondary);
-    color: var(--supp2);
+    color: var(--primary);
     border: none;
     border-radius: 5px;
     margin-top: 15px;
     cursor: pointer;
     box-shadow: 4px 3px 3px rgba(0, 0, 0, 0.2);
 `
+const FormRegister = styled.form`
+    width: 100%;
+    height: 100%;
+`
 export const ModalForm = () => {
+    const [dominio, setDominio] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [url, setUrl] = useState('')
+
+    const handleSubmit = async (e: React.FormEvent) => {
+
+        e.preventDefault();
+
+        try {
+
+            if (!dominio || !email || !password) {
+                alert('preencha todos os campos')
+            }
+
+            const token = localStorage.getItem('token')
+
+            const response = await axios.post('http://localhost:8081/registers/new', {
+                dominio,
+                email,
+                password,
+                url
+            },
+        {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+            console.log(response)
+        }
+        catch(err : any) {
+                console.log(err)
+                alert(err.response?.data?.message || 'Erro ao registrar nova conta')
+        }
+    }
     return(
+      <FormRegister onSubmit={handleSubmit}>
         <ContainerForm>
-            {formLogin.map((item, i) => (
-            <SubContainerForm key={i}>
-                <TitleForm>{item.title}</TitleForm>
-                <InputForm key={i} placeholder={item.title.toLocaleLowerCase()}></InputForm>
-            </SubContainerForm>  
-            ))} 
-            <PasswordButton>Gerar Senha</PasswordButton>
-            <RegisterButton>Registrar</RegisterButton>
+            <TitleForm>Registrar nova conta</TitleForm>
+            <SubContainerForm>
+                <InputForm type="text" placeholder="Dominio" value={dominio} onChange={(e) => setDominio(e.target.value)}/>
+            </SubContainerForm>
+            <SubContainerForm>
+                <InputForm type="email" placeholder="E-mail:" value={email} onChange={(e) => setEmail(e.target.value)}/>
+            </SubContainerForm>
+            <SubContainerForm>
+                <InputForm type="password" placeholder="Senha:" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </SubContainerForm>
+             <SubContainerForm>
+                <InputForm type="text" placeholder="Url da imagem:" value={url} onChange={(e) => setUrl(e.target.value)}/>
+            </SubContainerForm>
+            <RegisterButton type="submit"> Registrar </RegisterButton>
         </ContainerForm>
+      </FormRegister>
     )
 }
