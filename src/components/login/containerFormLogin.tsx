91486d23a5d5e61318a60092e27f90a2f8f0c653
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-08-02 10:31:54 -0300

    teste deploy

--- src/components/login/containerFormLogin.tsx
+++ src/components/login/containerFormLogin.tsx
@@ -88,7 +88,7 @@ export const ContainerFormLogin = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('http://localhost:8081/users/log', {
+            const response = await axios.post('passwordguardnew-backend-production.up.railway.app/users/log', {
                 email,
                 password
             })
