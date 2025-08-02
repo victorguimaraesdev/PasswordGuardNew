2eeafffa7e62d6d2b35f7a53b9a36c2620869eef
victorguimaraesdev <victorguimaraesmax@gmail.com>
2025-08-02 11:34:07 -0300

    cadastro

--- src/components/login/modalFormRegister.tsx
+++ src/components/login/modalFormRegister.tsx
@@ -74,7 +74,7 @@ export const ModalFormLoginRegister: React.FC<ModalFormLoginRegisterProps> = ({o
         }
 
         try {
-            const response = await axios.post('http://localhost:8081/users/reg', {
+            const response = await axios.post('passwordguardnew-backend-production.up.railway.app/users/reg', {
                 name,
                 email,
                 password
